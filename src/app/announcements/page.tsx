6dbdd6c5ec010b1441f8180b976072736fb8fb7d
Rasmus <rasmusolssony@gmail.com>
2025-03-06 19:58:25 +0100

    fix: formatting for inital pages

--- src/app/announcements/page.tsx
+++ src/app/announcements/page.tsx
@@ -2,7 +2,13 @@ import { Box, Heading, Text } from '@chakra-ui/react';
 
 const Announcements = () => {
   return (
-    <Box>
+    <Box
+      p={6}
+      textAlign="center"
+      bg="gray.900"
+      color="white"
+      minH="calc(100vh - 64px)"
+    >
       <Heading as="h1" size="4xl">
         Announcements
       </Heading>
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -2,7 +2,13 @@ import { Box, Heading, Text } from '@chakra-ui/react';
 
 export const Members = () => {
   return (
-    <Box>
+    <Box
+      p={6}
+      textAlign="center"
+      bg="gray.900"
+      color="white"
+      minH="calc(100vh - 64px)"
+    >
       <Heading as="h1" size="4xl">
         Members
       </Heading>

3233d81ad0eae6bfc547ab08bd379b725959333e
Rasmus <rasmusolssony@gmail.com>
2025-05-13 17:49:52 +0200

    fix: remove exportPathMap from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ['@chakra-ui/react'],
   },
   output: 'export',
-  exportPathMap: async (defaultPathMap) => {
-    const { 'api/discord/callback': _, ...filteredPaths } = defaultPathMap;
-    return filteredPaths;
-  },
   images: {
     unoptimized: true,
   },
